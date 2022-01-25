37b7902c5786b416a876aca3150274a6ab52764d
Kingsley Eneja <eneja.kc@gmail.com>
2022-01-25 15:34:53 +0100

    - Create admin sales listing feature

---
+++ db/migrate/20220125140918_create_sales.rb
@@ -0,0 +1,13 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.string :name
+      t.date :starts_on
+      t.date :ends_on
+      t.string :status
+      t.decimal :percent_off
+
+      t.timestamps null: false
+    end
+  end
+end
