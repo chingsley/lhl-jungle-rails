624f31fe92fbaa8844d1df6115887bb446d9d11c
Kingsley Eneja <eneja.kc@gmail.com>
2022-02-01 08:51:32 +0100

    - Test the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    pp user
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      # puts "Invalid user input"
+      puts user.errors.full_messages
       redirect_to '/signup'
     end
   end
